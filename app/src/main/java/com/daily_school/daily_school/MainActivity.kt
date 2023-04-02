51ff46eb28fc9937c8b8d3655f32fc5c4e1aa708
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-04-02 23:44:42 +0900

    init project

---
+++ app/src/main/java/com/daily_school/daily_school/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.daily_school.daily_school
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
