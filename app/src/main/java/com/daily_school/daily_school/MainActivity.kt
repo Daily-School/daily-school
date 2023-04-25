7a1bb24f6c6890465da314c1c4bc54400ca11265
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-04-25 16:41:30 +0900

    Feat: kakao 로그아웃 기능 구현

--- app/src/main/java/com/daily_school/daily_school/MainActivity.kt
+++ app/src/main/java/com/daily_school/daily_school/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.daily_school.daily_school
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.daily_school.daily_school.databinding.ActivityMainBinding
+import com.daily_school.daily_school.ui.LoginActivity
+import com.kakao.sdk.user.UserApiClient
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.mainBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.mainKakaoBtn.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                }
+                else {
+                    Log.i(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                }
+            }
+        }
+
     }
 }
