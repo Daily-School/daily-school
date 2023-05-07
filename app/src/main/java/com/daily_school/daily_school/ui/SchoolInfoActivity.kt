79861c8dcfcfdecb2089a8709b6388b075101cd4
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-05-07 14:08:06 +0900

    Feat: SchoolInfoActivity 임시 파일

---
+++ app/src/main/java/com/daily_school/daily_school/ui/SchoolInfoActivity.kt
@@ -0,0 +1,27 @@
+package com.daily_school.daily_school.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.EditText
+import com.daily_school.daily_school.R
+
+class SchoolInfoActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_school_info)
+
+//        val editTxt = findViewById<EditText>(R.id.schoolInfoNameEdit)
+//
+////        editTxt.setOnClickListener {
+////            val bottomSheet = SchoolNameFragment()
+////            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+////        }
+
+    }
+
+}
