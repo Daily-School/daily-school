917e8ff2cdc132903547f1851ead6ab901af3927
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-05-10 12:47:38 +0900

    Feat: 학년/반 스피너 기능 구현

--- app/src/main/java/com/daily_school/daily_school/ui/SchoolInfoActivity.kt
+++ app/src/main/java/com/daily_school/daily_school/ui/SchoolInfoActivity.kt
@@ -1,26 +1,111 @@
 package com.daily_school.daily_school.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.widget.EditText
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
 import com.daily_school.daily_school.R
+import com.daily_school.daily_school.databinding.ActivitySchoolInfoBinding
+
 
 class SchoolInfoActivity : AppCompatActivity() {
 
+    private var _binding: ActivitySchoolInfoBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var spinnerAdapterGrade : StudentInfoSpinnerAdapter
+    private lateinit var spinnerAdapterClass : StudentInfoSpinnerAdapter
+    private val listOfGrade = ArrayList<StudentInfoSpinnerModel>()
+    private val listOfClass = ArrayList<StudentInfoSpinnerModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_school_info)
+        _binding = ActivitySchoolInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        val editTxt = findViewById<EditText>(R.id.schoolInfoNameEdit)
-//
-////        editTxt.setOnClickListener {
-////            val bottomSheet = SchoolNameFragment()
-////            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
-////        }
+        // 학교 검색 fragment 호출 함수
+        showSearchFragment()
+
+        // 학년 스피너 셋업 함수
+        setupSpinnerGrade()
+
+        // 반 스피너 셋업 함수
+        setupSpinnerClass()
+
+        // 스피너 핸들러 셋업 함수
+        setupSpinnerHandler()
+    }
+
+    // 학교 검색 fragment 호출 함수
+    private fun showSearchFragment() {
+        binding.schoolInfoNameEdit.setOnClickListener {
+            val bottomSheet = SchoolNameFragment()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
+    }
+
+    // 학년 스피너 셋업 함수
+    private fun setupSpinnerGrade() {
+        val sGrades = resources.getStringArray(R.array.spinner_grade)
+
+        for (i in sGrades.indices){
+            val sGrade = StudentInfoSpinnerModel(sGrades[i])
+            listOfGrade.add(sGrade)
+        }
+        spinnerAdapterGrade = StudentInfoSpinnerAdapter(this, R.layout.item_student_info_spinner, listOfGrade)
+        binding.schoolInfoGradeSpinner.adapter = spinnerAdapterGrade
+    }
+
+    // 반 스피너 셋업 함수
+    private fun setupSpinnerClass() {
+        val sClasses = resources.getStringArray(R.array.spinner_class)
+
+        for (i in sClasses.indices){
+            val sClass = StudentInfoSpinnerModel(sClasses[i])
+            listOfClass.add(sClass)
+        }
+
+        spinnerAdapterClass = StudentInfoSpinnerAdapter(this, R.layout.item_student_info_spinner, listOfClass)
+        binding.schoolInfoClassSpinner.adapter = spinnerAdapterClass
+    }
+
+    // 스피너 핸들러 셋업 함수
+    private fun setupSpinnerHandler() {
+
+        binding.schoolInfoGradeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val bgShape = binding.schoolInfoGradeSpinner.background
+
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        binding.schoolInfoClassSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                binding.schoolInfoClassSpinner.setSelection(position, false)
+                
+            }
+
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                binding.schoolInfoClassSpinner.setBackgroundResource(R.drawable.bg_spinner_blue)
+            }
+        }
 
     }
 
