7ad9096266cbb9fd8f92d68a7951b69fce379f4d
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-05-10 12:52:33 +0900

    Feat: BottomSheetDialogFragment 기능 구현

---
+++ app/src/main/java/com/daily_school/daily_school/ui/SchoolNameFragment.kt
@@ -0,0 +1,78 @@
+package com.daily_school.daily_school.ui
+
+import android.app.Activity
+import android.app.Dialog
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.daily_school.daily_school.R
+import com.daily_school.daily_school.databinding.FragmentSchoolNameBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class SchoolNameFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding : FragmentSchoolNameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        return inflater.inflate(R.layout.fragment_school_name, container, false)
+
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {
+            val bottomSheetDialog = it as BottomSheetDialog
+            // 다이얼로그 크기 설정 (인자값 : DialogInterface)
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+    /** setupRatio()
+     * bottomSheet
+     *      - 전달받은 DialogInterface를 통해 View를 참조하도록 한다.
+     *      - 이때 id 값에는 내가 만든 View가 들어가는게 아님을 주의하자.
+     * layoutParams
+     *      - View를 감싸고 있는 Parents에게 어떻게 레이아웃 할 것인지 설정하는데 사용함.
+     * behavior
+     *      - 생성한 View를 통해 BottomSheetBehavior를 생성한다.
+     *      - 역할 : BottomSheetBehavior로 상태값을 '확장형'으로 설정해준다.
+     * */
+    // BottomSheetDialog Interface를 view에 전달하는 함수
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
+        //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
+        //id = android.support.design.R.id.design_bottom_sheet for support librares
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from<View>(bottomSheet)
+        val layoutParams = bottomSheet!!.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    // BottomSheetDialog 높이 조절 함수
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 90 / 100
+    }
+
+    // Window 크기 조절 함수
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+
+}
