68289789e9006a1c575dce45f55224ef130463b6
JeHeeYu <sees1234558@gmail.com>
2023-05-19 14:16:42 +0900

    Feat: Home 화면 UI 구현

--- app/src/main/java/com/daily_school/daily_school/utils/NaverRef.kt
+++ app/src/main/java/com/daily_school/daily_school/utils/NaverRef.kt
@@ -1,7 +1,7 @@
 package com.daily_school.daily_school.utils
 
 object NaverRef {
-    const val naverClientId = "MrpE48dQYMWLkmn9JyiK"
-    const val naverClientSecret = "Xn13swFYFg"
+    const val naverClientId = ""
+    const val naverClientSecret = ""
     const val naverClientName = "데일리스쿨"
 }
