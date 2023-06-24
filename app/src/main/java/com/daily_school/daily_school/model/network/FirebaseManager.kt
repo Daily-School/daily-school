d59080db582ddcc5ac22071c86b9d144df04b72b
JeHeeYu <sees1234558@gmail.com>
2023-06-24 16:11:54 +0900

    Feat: 파이어베이스 유저 데이터 저장

--- app/src/main/java/com/daily_school/daily_school/model/network/FirebaseManager.kt
+++ app/src/main/java/com/daily_school/daily_school/model/network/FirebaseManager.kt
@@ -1,7 +1,29 @@
 import android.util.Log
-//import com.google.firebase.FirebaseApp
-//import com.google.firebase.database.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class FirebaseManager {
+    private val TAG = "FirebaseManager"
 
+    private val usersCollection = "users"
+
+    fun saveCurrentUser(uid: String) {
+        val db = FirebaseFirestore.getInstance()
+        val userDocRef = db.collection(usersCollection).document(uid)
+
+        val currentTime = Calendar.getInstance().time as Any
+
+        val data = mutableMapOf<String, Any>()
+        data["time"] = currentTime
+
+        userDocRef.set(data)
+            .addOnSuccessListener {
+                Log.d(TAG, "Time saved successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Failed to save time", e)
+            }
+    }
 }
