e1d2484c0a61828377987d8275cb2bd68ab2072b
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-06-26 22:28:20 +0900

    Feat: retrofit 설정

---
+++ app/src/main/java/com/daily_school/daily_school/model/network/RetrofitApi.kt
@@ -0,0 +1,29 @@
+package com.daily_school.daily_school.model.network
+
+import com.daily_school.daily_school.ui.search.SchoolInfoService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitApi {
+    private const val BASE_URL = "https://open.neis.go.kr/hub/"
+
+    private val okHttpClient : OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+    private val retrofit : Retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+    val schoolInfoService : SchoolInfoService by lazy {
+        retrofit.create(SchoolInfoService::class.java)
+    }
+}
