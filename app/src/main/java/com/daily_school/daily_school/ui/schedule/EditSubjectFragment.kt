d881910236dbcc9f62171ecf7306391096087f90
JeHeeYu <sees1234558@gmail.com>
2023-07-06 10:47:16 +0900

    Feat: 과목 수정 프래그먼트 추가

---
+++ app/src/main/java/com/daily_school/daily_school/ui/schedule/EditSubjectFragment.kt
@@ -0,0 +1,75 @@
+package com.daily_school.daily_school.ui.schedule
+
+import android.app.Activity
+import android.app.Dialog
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.daily_school.daily_school.R
+import com.daily_school.daily_school.databinding.FragmentEditSubjectBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class EditSubjectFragment : BottomSheetDialogFragment() {
+    private lateinit var binding : FragmentEditSubjectBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit_subject, container, false)
+
+        // 프래그먼트를 종료하는 함수 호출
+        finishFragment()
+
+        return binding.root
+
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {
+            val bottomSheetDialog = it as BottomSheetDialog
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+    // BottomSheetDialog Interface를 view에 전달하는 함수
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from<View>(bottomSheet)
+        val layoutParams = bottomSheet!!.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    // BottomSheetDialog 높이 조절 함수
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight()// * 100 / 100
+    }
+
+    // Window 크기 조절 함수
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+    // 프래그먼트를 종료하는 함수
+    private fun finishFragment(){
+
+        binding.editSubjectIcBack.setOnClickListener {
+
+            dismiss()
+
+        }
+    }
+}
