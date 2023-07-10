70fac97415dcf44d1edfa685122e79e063ee6bca
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-07-10 21:53:23 +0900

    Feat: 점심 메뉴 불러오는 기능 구현

--- app/src/main/java/com/daily_school/daily_school/ui/meal/launch/MealLaunchTodayDialogFragment.kt
+++ app/src/main/java/com/daily_school/daily_school/ui/meal/launch/MealLaunchTodayDialogFragment.kt
@@ -1,10 +1,12 @@
 package com.daily_school.daily_school.ui.meal.launch
 
+import FirebaseManager
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,15 +14,32 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.daily_school.daily_school.R
 import com.daily_school.daily_school.databinding.FragmentMealLaunchTodayDialogBinding
+import com.daily_school.daily_school.model.network.RetrofitApi
 import com.daily_school.daily_school.ui.meal.breakfast.MealBreakfastTodayDialogRvAdapter
 import com.daily_school.daily_school.ui.meal.breakfast.MealBreakfastTodayModel
+import com.daily_school.daily_school.utils.KakaoRef
+import com.daily_school.daily_school.utils.NeisRef
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MealLaunchTodayDialogFragment : DialogFragment() {
 
+    private var TAG = MealLaunchTodayDialogFragment::class.java.simpleName
+
     private lateinit var binding : FragmentMealLaunchTodayDialogBinding
+
+    private var firebaseManager = FirebaseManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,21 +82,90 @@ class MealLaunchTodayDialogFragment : DialogFragment() {
 
     private fun todayMeal(){
 
-        val todayItems = ArrayList<MealLaunchTodayModel>()
+        val service = RetrofitApi.mealInfoService
 
-        todayItems.add(MealLaunchTodayModel("치킨마요 덮밥"))
-        todayItems.add(MealLaunchTodayModel("단무지"))
-        todayItems.add(MealLaunchTodayModel("탕수육"))
-        todayItems.add(MealLaunchTodayModel("쿨피스"))
-        todayItems.add(MealLaunchTodayModel("소고기 무국"))
+        val todayTime = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Calendar.getInstance().timeInMillis)
 
-        val todayLaunchDialogRvAdapter = MealLaunchTodayDialogRvAdapter(todayItems)
+        val todayDialogTime = SimpleDateFormat("MM월 dd일 (E)", Locale.getDefault()).format(Calendar.getInstance().timeInMillis)
 
-        val todayLaunchDialogRv = binding.mealLaunchTodayDialogRv
+        KakaoSdk.init(requireContext(), KakaoRef.APP_KEY)
 
-        todayLaunchDialogRv.adapter = todayLaunchDialogRvAdapter
+        UserApiClient.instance.accessTokenInfo{ tokenInfo, error ->
+            if (error != null){
+                Log.e(TAG, "토큰 정보 보기 실패", error)
+            }
+            else if (tokenInfo != null){
+                lifecycleScope.launch{
+                    try {
+                        val userInfo = firebaseManager.readSchoolInfoData(tokenInfo.id.toString())
+                        if (userInfo != null) {
+                            val userCityCode = userInfo["cityCode"]
+                            val userSchoolCode = userInfo["schoolCode"]
 
-        todayLaunchDialogRv.layoutManager = GridLayoutManager(requireContext(), 1)
+                            UserApiClient.instance.me { user, error ->
+                                if (error != null) {
+                                    Log.e(TAG, "사용자 정보 요청 실패 $error")
+                                } else if (user != null) {
+                                    Log.e(TAG, "사용자 정보 요청 성공 : $user")
+
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        val response = service.todayMealData( "json", 1,100, userCityCode.toString(), userSchoolCode.toString()
+                                            , todayTime, todayTime, NeisRef.api_key)
+
+                                        withContext(Dispatchers.Main) {
+                                            if (response.isSuccessful) {
+
+                                                if(response.body()?.mealServiceDietInfo?.get(1)?.row?.get(0)?.mMEALSCNM.toString() == "중식"){
+                                                    val result = response.body()?.mealServiceDietInfo?.get(1)?.row?.get(0)?.dDISHNM.toString()
+
+                                                    var todayLaunchMenu = result
+
+                                                    val arr = todayLaunchMenu.split("<br/>")
+
+                                                    if(arr.size <= 1){
+                                                        binding.mealLaunchDialogNotionIc.visibility = View.VISIBLE
+                                                        binding.mealLaunchNotionDialogTextView.visibility = View.VISIBLE
+                                                        binding.mealLaunchTodayDialogRv.visibility = View.INVISIBLE
+                                                    }else{
+                                                        val todayItems = ArrayList<MealLaunchTodayModel>()
+
+                                                        for (element in arr) {
+                                                            todayItems.add(MealLaunchTodayModel(element))
+                                                        }
+
+                                                        binding.mealLaunchTodayDialogDateTxt.text = todayDialogTime
+
+                                                        binding.mealLaunchDialogNotionIc.visibility = View.INVISIBLE
+                                                        binding.mealLaunchNotionDialogTextView.visibility = View.INVISIBLE
+                                                        binding.mealLaunchTodayDialogRv.visibility = View.VISIBLE
+
+                                                        val todayLaunchDialogRvAdapter = MealLaunchTodayDialogRvAdapter(todayItems)
+                                                        val todayLaunchDialogRv = binding.mealLaunchTodayDialogRv
+                                                        todayLaunchDialogRv.adapter = todayLaunchDialogRvAdapter
+                                                        todayLaunchDialogRv.layoutManager = GridLayoutManager(requireContext(), 1)
+                                                    }
+                                                }
+
+                                            } else {
+                                                Log.e(TAG, response.code().toString())
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+
+                        } else {
+                            Log.d(TAG, "TodoList is Null")
+                        }
+                    }
+                    catch (e: Exception) {
+                        Log.e(TAG, "Failed to Read", e)
+                    }
+                }
+
+            }
+        }
     }
 
     private fun fragmentFinish(){
--- app/src/main/java/com/daily_school/daily_school/ui/page/MealLaunchFragment.kt
+++ app/src/main/java/com/daily_school/daily_school/ui/page/MealLaunchFragment.kt
@@ -1,19 +1,34 @@
 package com.daily_school.daily_school.ui.page
 
+import FirebaseManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.daily_school.daily_school.R
 import com.daily_school.daily_school.databinding.FragmentMealLaunchBinding
+import com.daily_school.daily_school.model.network.RetrofitApi
 import com.daily_school.daily_school.ui.meal.MealCalendarFragment
+import com.daily_school.daily_school.ui.meal.Row
 import com.daily_school.daily_school.ui.meal.breakfast.MealBreakfastTodayDialogFragment
 import com.daily_school.daily_school.ui.meal.dinner.MealDinnerTodayDialogFragment
 import com.daily_school.daily_school.ui.meal.launch.*
+import com.daily_school.daily_school.utils.KakaoRef
+import com.daily_school.daily_school.utils.NeisRef
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -21,9 +36,13 @@ import kotlin.collections.ArrayList
 
 class MealLaunchFragment : Fragment() {
 
+    private var TAG = MealLaunchFragment::class.java.simpleName
+
     private var currentDate = LocalDate.now()
 
     private lateinit var binding : FragmentMealLaunchBinding
+
+    private var firebaseManager = FirebaseManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,7 +88,7 @@ class MealLaunchFragment : Fragment() {
     // 다이얼로그 프래그먼트 보여주는 함수
     private fun showFragment(){
         binding.mealLaunchIcToMealDialog.setOnClickListener {
-            val bottomSheet = MealDinnerTodayDialogFragment()
+            val bottomSheet = MealLaunchTodayDialogFragment()
             bottomSheet.show(requireActivity().supportFragmentManager, bottomSheet.tag)
         }
     }
@@ -84,30 +103,93 @@ class MealLaunchFragment : Fragment() {
 
     // 오늘의 급식 함수
     private fun todayMeal(){
+        val service = RetrofitApi.mealInfoService
 
-        val todayItems = ArrayList<MealLaunchTodayModel>()
+        val todayTime = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(Calendar.getInstance().timeInMillis)
 
-        todayItems.add(MealLaunchTodayModel("치킨마요 덮밥"))
-        todayItems.add(MealLaunchTodayModel("단무지"))
-        todayItems.add(MealLaunchTodayModel("탕수육"))
-        todayItems.add(MealLaunchTodayModel("쿨피스"))
-        todayItems.add(MealLaunchTodayModel("소고기 무국"))
+        KakaoSdk.init(requireContext(), KakaoRef.APP_KEY)
 
-        // 데이터가 있으면 리사이클러뷰의 상태를 visible로 변환
-        if (todayItems.isNotEmpty()){
-            binding.mealLaunchNotionIc.visibility = View.INVISIBLE
-            binding.mealLaunchNotionTextView.visibility = View.INVISIBLE
-            binding.todayLaunchMealRv.visibility = View.VISIBLE
+        UserApiClient.instance.accessTokenInfo{ tokenInfo, error ->
+            if (error != null){
+                Log.e(TAG, "토큰 정보 보기 실패", error)
+            }
+            else if (tokenInfo != null){
+                lifecycleScope.launch{
+                    try {
+                        val userInfo = firebaseManager.readSchoolInfoData(tokenInfo.id.toString())
+                        if (userInfo != null) {
+                            val userCityCode = userInfo["cityCode"]
+                            val userSchoolCode = userInfo["schoolCode"]
 
+                            UserApiClient.instance.me { user, error ->
+                                if (error != null) {
+                                    Log.e(TAG, "사용자 정보 요청 실패 $error")
+                                } else if (user != null) {
+                                    Log.e(TAG, "사용자 정보 요청 성공 : $user")
+
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        val response = service.todayMealData( "json", 1,100, userCityCode.toString(), userSchoolCode.toString(), todayTime, todayTime,NeisRef.api_key)
+
+                                        withContext(Dispatchers.Main) {
+                                            if (response.isSuccessful) {
+
+                                                if(response.body()?.mealServiceDietInfo?.get(1)?.row?.get(0)?.mMEALSCNM.toString() == "중식"){
+                                                    val result = response.body()?.mealServiceDietInfo?.get(1)?.row?.get(0)?.dDISHNM.toString()
+
+                                                    val cal = response.body()?.mealServiceDietInfo?.get(1)?.row?.get(0)?.cALINFO.toString()
+
+                                                    var todayLaunchMenu = result
+
+                                                    val arr = todayLaunchMenu.split("<br/>")
+
+                                                    if (arr.size <= 1){
+                                                        binding.mealLaunchNotionIc.visibility = View.VISIBLE
+                                                        binding.mealLaunchNotionTextView.visibility = View.VISIBLE
+                                                        binding.todayLaunchMealRv.visibility = View.INVISIBLE
+                                                    }
+                                                    else{
+                                                        val todayItems = ArrayList<MealLaunchTodayModel>()
+
+                                                        for (element in arr) {
+
+                                                            todayItems.add(MealLaunchTodayModel(element))
+                                                        }
+
+                                                        binding.mealLaunchNotionIc.visibility = View.INVISIBLE
+                                                        binding.mealLaunchNotionTextView.visibility = View.INVISIBLE
+                                                        binding.todayLaunchMealRv.visibility = View.VISIBLE
+
+                                                        binding.mealLaunchCalorieTextView.text = cal
+
+                                                        val todayLaunchRvAdapter = MealLaunchTodayRvAdapter(todayItems)
+                                                        val todayLaunchRv = binding.todayLaunchMealRv
+                                                        todayLaunchRv.adapter = todayLaunchRvAdapter
+                                                        todayLaunchRv.layoutManager = GridLayoutManager(requireContext(), 2)
+                                                    }
+                                                }
+
+
+                                            } else {
+                                                Log.e(TAG, response.code().toString())
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+
+                        } else {
+                            Log.d(TAG, "TodoList is Null")
+                        }
+                    }
+                    catch (e: Exception) {
+                        Log.e(TAG, "Failed to Read", e)
+                    }
+                }
+
+            }
         }
 
-        val todayLaunchRvAdapter = MealLaunchTodayRvAdapter(todayItems)
-
-        val todayLaunchRv = binding.todayLaunchMealRv
-
-        todayLaunchRv.adapter = todayLaunchRvAdapter
-
-        todayLaunchRv.layoutManager = GridLayoutManager(requireContext(), 2)
     }
 
     // 이번주 급식 함수
