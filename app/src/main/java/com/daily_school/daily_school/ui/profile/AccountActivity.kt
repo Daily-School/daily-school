e7d5682774a0bd50447f996cbc9d2e748a7d150a
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-08-03 15:42:18 +0900

    Feat: 사용자 카카오 계정 닉네임 불러오는 기능 구현

--- app/src/main/java/com/daily_school/daily_school/ui/profile/AccountActivity.kt
+++ app/src/main/java/com/daily_school/daily_school/ui/profile/AccountActivity.kt
@@ -1,18 +1,25 @@
 package com.daily_school.daily_school.ui
 
+import FirebaseManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.daily_school.daily_school.R
 import com.daily_school.daily_school.databinding.ActivityAccountBinding
 import com.daily_school.daily_school.ui.profile.AccountDialog
+import com.daily_school.daily_school.utils.KakaoRef
+import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.launch
 
 class AccountActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityAccountBinding
+
+    private var TAG = AccountActivity::class.java.simpleName
 
     val manager = supportFragmentManager
 
@@ -23,6 +30,9 @@ class AccountActivity : AppCompatActivity() {
 
         // 프래그먼트 화면으로 돌아가는 함수 호출
         backToProfile()
+
+        // 카카오 계정 정보 불러오는 함수 호출
+        loadUserInfo()
 
         // 회원 탈퇴 기능 함수 호출
         unlinkBtn()
@@ -37,6 +47,37 @@ class AccountActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    // 카카오 계정 정보 불러오는 함수
+    private fun loadUserInfo(){
+        KakaoSdk.init(this, KakaoRef.APP_KEY)
+
+        UserApiClient.instance.accessTokenInfo{ tokenInfo, error ->
+            if (error != null){
+                Log.e(TAG, "토큰 정보 보기 실패", error)
+            }
+            else if (tokenInfo != null){
+                lifecycleScope.launch{
+                    try {
+                        UserApiClient.instance.me { user, error ->
+                            if (error != null) {
+                                Log.e(TAG, "사용자 정보 요청 실패 $error")
+                            } else if (user != null) {
+                                Log.e(TAG, "사용자 정보 요청 성공 : $user")
+                                val nickname = user.kakaoAccount?.profile?.nickname.toString()
+                                Log.d(TAG, nickname)
+                                binding.accountCurrentAccountDynamicTextView.text = "@$nickname"
+                            }
+                        }
+                    }
+                    catch (e: Exception) {
+                        Log.e(TAG, "Failed to Read", e)
+                    }
+                }
+
+            }
+        }
     }
 
     // 회원 탈퇴 기능 함수
