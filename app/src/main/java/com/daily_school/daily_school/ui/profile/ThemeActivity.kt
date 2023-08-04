2b56fecb35bf05ae32cd481b399d252cfd9c1e33
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-08-04 17:21:01 +0900

    Fix: 다크 모드 체크 아이콘 수정

--- app/src/main/java/com/daily_school/daily_school/ui/profile/ThemeActivity.kt
+++ app/src/main/java/com/daily_school/daily_school/ui/profile/ThemeActivity.kt
@@ -2,6 +2,7 @@ package com.daily_school.daily_school.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.daily_school.daily_school.R
@@ -16,25 +17,23 @@ class ThemeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_theme)
 
-        lightMode()
+        binding.lightModeLayout.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding.themeIcLightMode.visibility = View.VISIBLE
+            binding.themeIcDarkMode.visibility = View.GONE
 
-        darkMode()
+        }
+
+        binding.darkModeLayout.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            binding.themeIcLightMode.visibility = View.GONE
+            binding.themeIcDarkMode.visibility = View.VISIBLE
+
+        }
 
         // 프래그먼트 화면으로 돌아가는 함수 호출
         backToProfile()
 
-    }
-
-    private fun lightMode(){
-        binding.lightModeLayout.setOnClickListener {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        }
-    }
-
-    private fun darkMode(){
-        binding.darkModeLayout.setOnClickListener {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
     }
 
     // 프래그먼트 화면으로 돌아가는 함수
